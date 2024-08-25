cb5a322fe4f26b82f57cf479becabee6dcc2970f
Jakub Janeček <kuba.rone@gmail.com>
2024-08-25 16:48:29 +1000

    Add profile page

--- src/controllers/webClientController.ts
+++ src/controllers/webClientController.ts
@@ -1,6 +1,6 @@
 import type { Request, Response } from 'express';
-import path from 'path';
 import { generateAccessToken } from '../services/authService';
+import { findPreferences, updatePreferences } from '../repositories/preferencesRepository';
 
 export const loginGet = (req: Request, res: Response) => {
    res.render("login");
@@ -30,4 +30,39 @@ export const logout = (req: Request, res: Response) => {
 
 export const home = (req: Request, res: Response) => {
    res.render("home");
+}
+
+export const profileGet = async (req: Request, res: Response) => {
+   if (!req.user) {
+      return res.sendStatus(500);
+   }
+
+   const preferences = await findPreferences(req.user.id);
+
+   if (!preferences) {
+      return res.sendStatus(500);
+   }
+
+   res.render("profile", {
+      email: req.user.email,
+      fps: preferences.fps,
+      scale_x: preferences.scale_x === -1 ? "Auto" : preferences.scale_x,
+      scale_y: preferences.scale_y === -1 ? "Auto" : preferences.scale_y,
+   });
+}
+
+export const profilePost = async (req: Request, res: Response) => {
+   if (!req.user) {
+      return res.sendStatus(500);
+   }
+
+   const { fps, scale_x, scale_y } = req.body;
+
+   await updatePreferences(req.user.id, {
+      fps: fps,
+      scale_x: scale_x === "Auto" ? -1 : scale_x,
+      scale_y: scale_y === "Auto" ? -1 : scale_y,
+   });
+
+   res.redirect("/");
 }
--- src/routes/webClient.ts
+++ src/routes/webClient.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import path from 'path';
 import { authenticateCookie } from '../controllers/authController';
-import { loginGet, loginPost, logout, home } from '../controllers/webClientController';
+import { loginGet, loginPost, logout, home, profileGet, profilePost } from '../controllers/webClientController';
 
 const router = express.Router();
 
 router.get('/login', loginGet);
 router.post('/login', loginPost);
 router.get('/logout', authenticateCookie, logout);
+router.get('/profile', authenticateCookie, profileGet);
+router.post('/profile', authenticateCookie, profilePost);
 router.get('/', authenticateCookie, home);
 
 router.use("/", express.static(path.join(__dirname, "../../public")));
